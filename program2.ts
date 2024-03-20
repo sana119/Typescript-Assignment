b8a1b304e31121b2c38470eb233a73091ed37038
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:10:50 +0500

    Add files via upload

---
+++ program2.ts
@@ -0,0 +1,4 @@
+// exercise 10
+// my name is sana khursheed
+// Dated: 27/02/2024 
+console.log(5 * 2);
---
+++ q10.js
@@ -0,0 +1,5 @@
+"use strict";
+// exercise 10
+// my name is sana khursheed
+// Dated: 27/02/2024 
+console.log('hello world simple program');
