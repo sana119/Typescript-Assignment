7099eab543134d85aa54b7494f0198c2507421f5
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 00:59:53 +0500

    Add files via upload

---
+++ q3.ts
@@ -0,0 +1,15 @@
+// 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+
+let firstName: String = "Sana Khursheed";
+
+//lower case
+console.log("lowercase:",firstName.toLowerCase());
+//upper case
+console.log("uppercase:",firstName.toLocaleUpperCase());
+//titlecase
+console.log("titlecase:",firstName.replace(/\bw/g,c => c.toUpperCase()));
+
+
+
+
