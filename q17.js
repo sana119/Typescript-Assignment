644e4bca0ad5cfd9ec392b97a5acdf5e63ffed1f
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:18:03 +0500

    Add files via upload

---
+++ q17.js
@@ -0,0 +1,27 @@
+"use strict";
+// exercise 17
+let guest_list = ['Shagufta', 'Tariq', 'azim', 'sadia'];
+let new_Guest = 'Fatima';
+guest_list[0] = new_Guest;
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Dear . ' + guest_list[i] + ',\n\nit is our pleasure to invite you in our party . \n\nThank you!\n\n');
+}
+let absent_Guest = 'Sadia';
+console.log(`Mrs. ${absent_Guest} is not coming to the party. `);
+console.log('Good News! We find Big Table So we are inviting 3 more guests.');
+guest_list.unshift('Hira');
+guest_list.splice(2, 0, 'safia');
+guest_list.push('maryam');
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Dear . ' + guest_list[i] + ',\n\nit is our pleasure to invite you in our party . \n\nThank you!\n\n');
+}
+console.log('\nSorry we can not arrange big table,only Two peolpe well be invited.');
+while (guest_list.length > 2) {
+    let remove_Guest = guest_list.pop();
+    console.log(`Sorry . ${remove_Guest}, you are not invited  for Dinner.`);
+}
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('Dear . ' + guest_list[i] + ',\n\nYou are still invited . \n\nThank you!\n\n');
+}
+guest_list.splice(0, 2);
+console.log(guest_list);
