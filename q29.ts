36544969f57016b3fc8a8608b7d6d202a816816f
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:28:33 +0500

    Add files via upload

---
+++ q29.ts
@@ -0,0 +1,22 @@
+// exercise 29
+
+// Make a array of your three favorite fruits and call it favorite_fruits.
+// Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+let fevorite_fruits = ['apple', 'banana', 'mango'];
+if (fevorite_fruits.includes('apple')) {
+    console.log('I really like Apple!');
+}
+if (fevorite_fruits.includes('banana')) {
+    console.log('I really like Banana!');
+}
+if (fevorite_fruits.includes('mango')) {
+    console.log('I really like Mango!');
+}
+if (fevorite_fruits.includes('orange')) {
+    console.log('I really like Orange!');
+}
+if (fevorite_fruits.includes('strawberry')) {
+    console.log('I really like Strawberry!');
+}
+
+
