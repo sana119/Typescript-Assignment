e39f5ddb56aae011f4fa590444ffc98dcee0cb6f
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:12:54 +0500

    Add files via upload

---
+++ q11.js
@@ -0,0 +1,6 @@
+"use strict";
+// exercise 11
+let members = ['maha', 'habiba', 'nimrah', 'saba', 'safia'];
+for (let i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ q11.ts
@@ -0,0 +1,8 @@
+// exercise 11
+let members: string[] =['maha','habiba','nimrah','saba','safia'];
+for(let i=0; i<members.length; i++){
+    console.log(members[i]);
+}
+
+
+ 
