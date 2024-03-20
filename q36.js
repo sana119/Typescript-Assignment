a08bc11e17ea64fceb27e5c60164d60ef64a19f8
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:34:15 +0500

    Add files via upload

---
+++ q36.js
@@ -0,0 +1,8 @@
+"use strict";
+// exercise 36
+function make_shirt(size, text) {
+    console.log(`creating a ${size} shirt with the message: ${text}`);
+}
+make_shirt('large', 'Hello world');
+make_shirt('medium', 'Hello world');
+make_shirt('small', 'Hello world');
---
+++ q36.ts
@@ -0,0 +1,10 @@
+// exercise 36
+
+
+function make_shirt(size: string, text:string){
+    console.log(`creating a ${size} shirt with the message: ${text}`)
+}
+
+make_shirt('large', 'Hello world');
+make_shirt('medium', 'Hello world');
+make_shirt('small', 'Hello world');
