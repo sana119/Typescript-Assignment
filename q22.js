d41d00e6f8f427f91385957f698e921bfa8118d0
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:23:52 +0500

    Add files via upload

---
+++ q22.js
@@ -0,0 +1,5 @@
+"use strict";
+// exercise 22
+let array = ['safia', 'nimra', 'taha', 4, 5, 6];
+// console.log(array[6])
+console.log(array[5]);
---
+++ q22.ts
@@ -0,0 +1,6 @@
+// exercise 22
+
+let array : (string | Number) [] = ['safia','nimra','taha',4,5,6]
+// console.log(array[6])
+ 
+console.log(array[5])
