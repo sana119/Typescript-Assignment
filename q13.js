b0914eed4a806fc80359529f3f8b18a875f5d82b
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:15:00 +0500

    Add files via upload

---
+++ q13.js
@@ -0,0 +1,3 @@
+"use strict";
+let vehichles = ["Honda motercycle", " Honda city", "Hilux", "Audi"];
+vehichles.map((items) => console.log(`I would like to own a ${items}`));
---
+++ q13.ts
@@ -0,0 +1,3 @@
+// exercise 13
+let vehichles: string[] = ["Honda motercycle"," Honda city", "Hilux","Audi"];
+vehichles.map((items) => console.log(`I would like to own a ${items}`));
