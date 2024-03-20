cee6d164bf09b007f0c52fc9c1dc4b759c061188
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:26:29 +0500

    Add files via upload

---
+++ q26.ts
@@ -0,0 +1,17 @@
+// exersice 26
+let alien_color: string = "green";
+if (alien_color == 'green') {
+    console.log("Player just earned 5 points for shooting the alien");
+} 
+else{
+    console.log("Player just earned 10 points. ")
+}
+
+
+
+ alien_color = "red";
+if (alien_color == 'green') {
+    console.log("Player just earned 5 points for shooting the alien");
+}else{
+    console.log("Player just earned 10 points")
+}
