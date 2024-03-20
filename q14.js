82743fa1d2381d1940e533e6e2662bb006faf21c
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:15:43 +0500

    Add files via upload

---
+++ q14.js
@@ -0,0 +1,3 @@
+"use strict";
+let guestArr = ['shagufta', 'tariq', 'azim', 'sadia'];
+guestArr.map((items) => (console.log(`Dear ${items} , you are invited to the dinner `)));
---
+++ q14.ts
@@ -0,0 +1,7 @@
+// exercise 14
+let guestArr : string [] = ['shagufta','tariq','azim','sadia'];
+
+guestArr.map((items) => (console.log(`Dear ${items} , you are invited to the dinner `)));
+
+
+
