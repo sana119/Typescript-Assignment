ee34512acbb4fee52cc661f29f950e327ca5d7f5
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:22:16 +0500

    Add files via upload

---
+++ q20.js
@@ -0,0 +1,5 @@
+"use strict";
+// exercise 20
+let country = ['Pakistan', 'China', 'England', 'Japan', 'Canada'];
+console.log("List of country");
+console.log(country);
---
+++ q20.ts
@@ -0,0 +1,6 @@
+// exercise 20
+
+
+let country :string[] = ['Pakistan','China','England','Japan','Canada']; 
+console.log("List of country");
+console.log(country);
