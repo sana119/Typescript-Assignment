447f068255b791c00844fbdaed0b8ae95dda811e
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:25:54 +0500

    Add files via upload

---
+++ q25.ts
@@ -0,0 +1,17 @@
+// exercise 25
+
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+let alien_color = 'green'
+// if (alien_color == 'green') {
+//     console.log("Congratulations! you just earned 5 points.")
+// }
+
+// Second version
+alien_color = 'blue'
+ 
+if (alien_color == 'green') {
+    console.log("no output")
+}
+
+
