8e3b4769caf8a10e6e1dbc80976e687ee5c619cd
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:32:25 +0500

    Add files via upload

---
+++ q33.js
@@ -0,0 +1,17 @@
+"use strict";
+// exercise 33
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let number of numbers) {
+    if (number === 1) {
+        console.log(`${number}st`); //1st
+    }
+    else if (number === 2) {
+        console.log(`${number}nd`); //2nd
+    }
+    else if (number === 3) {
+        console.log(`${number}rd`); //3rd
+    }
+    else {
+        console.log(`${number}th`); // 4th , 5th , 6th ,7th , 8th , 9th
+    }
+}
