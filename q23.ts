89a30bd7634d4b6521dd7086a19e9321683956a7
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:24:43 +0500

    Add files via upload

---
+++ q23.ts
@@ -0,0 +1,52 @@
+// exercise 23
+let car = 'subaru';
+
+// test: 1 
+console.log("Is car === 'subaru'? I predict True.");
+
+console.log(car === 'subaru'); // True
+
+// test: 2 
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru'); // True
+
+
+  // test: 3
+console.log("Is car != 'Honda'? I predict True.");
+console.log(car != 'Honda'); // True
+
+ // test: 4 
+ console.log("Is car !== 'ford'? I predict true.");
+ console.log(car !== 'ford'); // True
+
+
+
+  // test: 5 
+  console.log("Is car.length == 6? predict True.");
+  console.log(car.length == 6);
+
+   // test: 6 
+  console.log("Is car == 'SABARU'? I predict False.");
+  console.log(car == 'SABARU'); 
+ 
+
+  
+  // test: 7 
+  console.log("Is car === 'SABARU'? I predict False.");
+  console.log(car === 'SABARU'); 
+
+
+ 
+  // test: 8 
+  console.log("Is car === 'Train'? I predict False.");
+  console.log(car ==='Train'); 
+
+  
+  // test: 9 
+  console.log("Is car === 'Bus'? I predict False.");
+  console.log(car === 'Bus'); 
+
+
+  // test: 10 
+  console.log("Is car === 'Cycle'? I predict False.");
+  console.log(car === 'Cycle'); 
