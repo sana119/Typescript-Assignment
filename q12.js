a888619f580eaa146ee6c43ac71c824fa4f6d412
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:14:26 +0500

    Add files via upload

---
+++ q12.js
@@ -0,0 +1,7 @@
+"use strict";
+// Exercise 12
+let members = ['maha', 'habiba', 'nimrah', 'saba', 'safia'];
+let message = 'hi how are you? what are you doing now a days:';
+for (let i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ q12.ts
@@ -0,0 +1,6 @@
+// Exercise 12
+let members: string[] =['maha','habiba','nimrah','saba','safia'];
+let message: string='hi how are you? what are you doing now a days:';
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
