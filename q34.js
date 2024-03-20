21327b5375073e989670f0914fbad30f36f9ea7e
sana119 <163931081+sana119@users.noreply.github.com>
2024-03-21 01:33:06 +0500

    Add files via upload

---
+++ q34.js
@@ -0,0 +1,7 @@
+"use strict";
+// exercise 34
+let piZZas = ['pepperoni', 'cuban', 'BBQ chicken'];
+for (let piZZa of piZZas) {
+    console.log(`I like ${piZZa} pizza.`);
+}
+console.log(`I really love pizza!`);
---
+++ q34.ts
@@ -0,0 +1,10 @@
+// exercise 34
+
+let piZZas: string[] = ['pepperoni','cuban','BBQ chicken'];
+
+for(let piZZa of piZZas){
+    console.log(`I like ${piZZa} pizza.`);
+
+}
+
+console.log(`I really love pizza!`);
